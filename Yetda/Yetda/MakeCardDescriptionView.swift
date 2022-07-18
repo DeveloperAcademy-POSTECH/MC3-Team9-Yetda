e39cd33184875df313b544f4da6f77a62b9059b7
Youngseo Yoon <58525719+yys88699@users.noreply.github.com>
2022-07-18 16:59:38 +0900

    Feat[#8] : 가로 Collection 구현

---
+++ Yetda/Yetda/MakeCardDescriptionView.swift
@@ -0,0 +1,27 @@
+//
+//  MakeCardDescriptionView.swift
+//  Yetda
+//
+//  Created by Youngseo Yoon on 2022/07/18.
+//
+
+import UIKit
+
+class MakeCardDescriptionView: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    var photos: [String] = ["photo1", "photo2", "photo3", "photo4", "photo5"]
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCollectionCell
+        cell.chosenPhoto.image = UIImage(named: photos[indexPath.row])
+        cell.layer.cornerRadius = 20.0
+        return cell
+    }
+}
