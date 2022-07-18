b78a2ab3a428deec7ffba50ebdef59b4ec677416
Jinsan Kim <gkjs1129@gmail.com>
2022-07-18 16:37:40 +0900

    Feat[#11] : ImageData를 UIImage Array로 만드는 코드작성

---
+++ Yetda/Yetda/CardDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CardDetailViewModel.swift
+//  Yetda
+//
+//  Created by Jinsan Kim on 2022/07/18.
+//
+
+import Foundation
--- Yetda/Yetda/Present+CoreDataProperties.swift
+++ Yetda/Yetda/Present+CoreDataProperties.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 
 extension Present {
@@ -29,7 +30,19 @@ extension Present {
     @NSManaged public var image4: Data?
     @NSManaged public var image5: Data?
     @NSManaged public var site: Site?
-
+    
+    public var imageArray: [UIImage] {
+        let imageList = [image1, image2, image3, image4, image5]
+        var result: [UIImage] = []
+        for image in imageList {
+            if let data = image {
+                result.append(UIImage(data: data)!)
+            } else {
+                break
+            }
+        }
+        return result
+    }
 }
 
 extension Present : Identifiable {
