58c310e43211d468533a0f158b508f9de14c9110
Valentino1994 <demua1994@gmail.com>
2022-07-22 15:59:18 +0900

    1

--- Yetda/Yetda/ViewController.swift
+++ Yetda/Yetda/ViewController.swift
@@ -8,31 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let isFirstLaunching: Bool = UserDefaults.standard.bool(forKey: "isFirstLaunching")
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        if Storage.isFirstTime() {
-            let onBoardingVC = OnBoardingViewController()
-            onBoardingVC.modalPresentationStyle = .fullScreen
-            
-            self.present(onBoardingVC, animated: true, completion: nil)
-        }
+        // Do any additional setup after loading the view.
     }
 }
 
-public class Storage {
-    static func isFirstTime() -> Bool {
-        let defaults = UserDefaults.standard
-        if defaults.object(forKey: "isFirstTime") == nil {
-            defaults.set("No", forKey:"isFirstTime")
-            return true
-        } else {
-            return false
-        }
-    }
-}
