0218da27db4a55d0f41ba3dc4eeac6ee466f0915
이채민 <94645868+shinehardd@users.noreply.github.com>
2022-07-25 15:29:03 +0900

    Feat[#20]: HomeView UI 완성했습니다!

--- Yetda/Yetda/SceneDelegate.swift
+++ Yetda/Yetda/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
                 let storyboard = UIStoryboard(name: "SiteCollectionView", bundle: nil)
                 let vc = storyboard.instantiateViewController(withIdentifier: "SiteViewController")
-                self.window?.rootViewController = vc
+                self.window?.rootViewController = HomeViewController()
                 self.window?.makeKeyAndVisible()
     }
 
