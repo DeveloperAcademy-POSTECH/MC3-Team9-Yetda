7ac7995c97d1fb029de947dee842378a84b2e061
Valentino1994 <demua1994@gmail.com>
2022-07-27 01:16:00 +0900

    Feat[#41] : 구글 로그인을 추가했습니다.

--- Yetda/Yetda/ViewController.swift
+++ Yetda/Yetda/ViewController.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
-
+import GoogleSignIn
 class ViewController: UIViewController {
     
     let isFirstLaunching: Bool = UserDefaults.standard.bool(forKey: "isFirstLaunching")
     
+    @IBOutlet weak var googleLoginButton: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -23,6 +27,11 @@ class ViewController: UIViewController {
             self.present(onBoardingVC, animated: true, completion: nil)
         }
     }
+    
+    @IBAction func googleLoginAction(_ sender: UIButton) {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
 }
 
 public class Storage {
