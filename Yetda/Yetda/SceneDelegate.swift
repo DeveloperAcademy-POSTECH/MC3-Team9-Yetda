9f7fbd6301d5aa87c60e661258648f9f30c081f1
boskim22 <boskim22@pos.idserve.net>
2022-07-28 17:31:42 +0900

    Feat: MyPageView의 UI를 구현했습니다.

--- Yetda/Yetda/SceneDelegate.swift
+++ Yetda/Yetda/SceneDelegate.swift
@@ -19,8 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // MARK: 여기서 자기가 띄우고 싶은 스토리보드랑 뷰컨트롤러를 설정해주면 됩니다.
         guard let _ = (scene as? UIWindowScene) else { return }
 
-                let storyboard = UIStoryboard(name: "MakeCard", bundle: nil)
-                let vc = storyboard.instantiateViewController(withIdentifier: "MakeCardDescriptionViewController")
+                let storyboard = UIStoryboard(name: "MyPageView", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "MyPageViewController")
                 self.window?.rootViewController = vc
                 self.window?.makeKeyAndVisible()
     }
