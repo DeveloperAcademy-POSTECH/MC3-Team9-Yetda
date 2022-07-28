2091a6caef75e11de2bd5fd17e603a6d29e3390d
Jinsan Kim <gkjs1129@gmail.com>
2022-07-28 17:37:59 +0900

    Feat[#17] 버튼기능 구현

--- Yetda/Yetda/CardDetail/CardDetailViewController.swift
+++ Yetda/Yetda/CardDetail/CardDetailViewController.swift
@@ -21,7 +21,6 @@ class CardDetailViewController: UIViewController, UIScrollViewDelegate {
     lazy var pageControl: UIPageControl = {
         let pageControl = UIPageControl()
 
-        pageControl.translatesAutoresizingMaskIntoConstraints = false
         pageControl.numberOfPages = pageSize
         pageControl.currentPage = 0
         pageControl.isUserInteractionEnabled = false
@@ -45,6 +44,19 @@ class CardDetailViewController: UIViewController, UIScrollViewDelegate {
         return scrollView
     }()
     
+    lazy var topContainerView: UIView = {
+        let screenWidth = self.view.frame.width
+        let screenHeight = self.view.frame.height
+        let topContainerView = UIView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenWidth))
+        
+        topContainerView.backgroundColor = .systemBackground
+        topContainerView.clipsToBounds = true
+        topContainerView.layer.cornerRadius = 20
+        topContainerView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
+        
+        return topContainerView
+    }()
+    
     lazy var cardDetailView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let cardDetailView = UICollectionView(frame: .zero, collectionViewLayout: layout)
@@ -52,7 +64,6 @@ class CardDetailViewController: UIViewController, UIScrollViewDelegate {
         cardDetailView.delegate = self
         cardDetailView.dataSource = self
         
-        cardDetailView.translatesAutoresizingMaskIntoConstraints = false
         cardDetailView.backgroundColor = .systemBackground
         cardDetailView.register(KeywordCell.self, forCellWithReuseIdentifier: "keywordCell")
         cardDetailView.register(ContentsCell.self, forCellWithReuseIdentifier: "contentsCell")
@@ -64,42 +75,40 @@ class CardDetailViewController: UIViewController, UIScrollViewDelegate {
     
     lazy var backButton: UIButton = {
         let button = makeButton(symbols: "chevron.backward")
+//        button.addTarget(self, action: #selector(moveToHomeView), for: .touchUpInside)
         
         return button
     }()
     
     lazy var shareButton: UIButton = {
         let button = makeButton(symbols: "square.and.arrow.up")
+//        button.addTarget(self, action: #selector(popoverModal), for: .touchUpInside)
         
         return button
     }()
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
-        navigationItem.title = "Detail"
         
-        let width = self.view.frame.width
-        let height = self.view.frame.height
-
-        let topContainerView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: width))
+        let screenWidth = self.view.frame.width
+        let screenHeight = self.view.frame.height
         
-        topContainerView.translatesAutoresizingMaskIntoConstraints = false
-        topContainerView.backgroundColor = .systemBackground
-        topContainerView.clipsToBounds = true
-        topContainerView.layer.cornerRadius = 20
-        topContainerView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
         self.view.addSubview(topContainerView)
-        
-        topContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        topContainerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        topContainerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        topContainerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -(height-width)).isActive = true
-        
-        pageControl.frame = CGRect(x: 0, y: 0, width: topContainerView.frame.maxX, height: 50)
+        setTopContainerViewConstraints(width: screenWidth, height: screenHeight)
         
         for i in 0 ..< dummyImages.count {
-            let imageView = UIImageView(frame: CGRect(x: CGFloat(i) * width, y: 0, width: width, height: topContainerView.frame.maxY))
+            let imageView = UIImageView(frame: CGRect(x: CGFloat(i) * screenWidth, y: 0, width: screenWidth, height: topContainerView.frame.height))
             imageView.image = UIImage(named: dummyImages[i])
             imageView.contentMode = .scaleToFill
             imageView.clipsToBounds = true
@@ -109,27 +118,16 @@ class CardDetailViewController: UIViewController, UIScrollViewDelegate {
             imageScrollView.addSubview(imageView)
         }
         
-        view.addSubview(cardDetailView)
-        cardDetailView.topAnchor.constraint(equalTo: topContainerView.bottomAnchor).isActive = true
-        cardDetailView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        cardDetailView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        cardDetailView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        
-        topContainerView.addSubview(self.imageScrollView)
-        topContainerView.addSubview(self.pageControl)
+        self.view.addSubview(cardDetailView)
+        topContainerView.addSubview(imageScrollView)
+        topContainerView.addSubview(pageControl)
         topContainerView.addSubview(backButton)
         topContainerView.addSubview(shareButton)
         
-        pageControl.topAnchor.constraint(equalTo: topContainerView.bottomAnchor, constant: -50).isActive = true
-        pageControl.centerXAnchor.constraint(equalTo: topContainerView.centerXAnchor).isActive = true
-        
-        backButton.topAnchor.constraint(equalTo: topContainerView.topAnchor, constant: 20).isActive = true
-        backButton.leadingAnchor.constraint(equalTo: topContainerView.leadingAnchor, constant: 20).isActive = true
-        backButton.trailingAnchor.constraint(equalTo: topContainerView.trailingAnchor, constant: -(width-60)).isActive = true
-        
-        shareButton.topAnchor.constraint(equalTo: topContainerView.topAnchor, constant: 20).isActive = true
-        shareButton.leadingAnchor.constraint(equalTo: topContainerView.leadingAnchor, constant: width-60).isActive = true
-        shareButton.trailingAnchor.constraint(equalTo: topContainerView.trailingAnchor, constant: -20).isActive = true
+        setCardDetailViewConstraints(topView: topContainerView)
+        setPageControlConstraints(topView: topContainerView)
+        setBackButtonConstraints(topView: topContainerView, width: screenWidth)
+        setShareButtonConstraints(topView: topContainerView, width: screenWidth)
     }
     
     override func didReceiveMemoryWarning() {
@@ -150,20 +148,66 @@ class CardDetailViewController: UIViewController, UIScrollViewDelegate {
         button.tintColor = .white
         button.configuration?.imagePadding = 0
         button.alpha = 0.5
-        button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
+    }
+    
+    func setTopContainerViewConstraints(width: CGFloat, height: CGFloat) {
+        topContainerView.translatesAutoresizingMaskIntoConstraints = false
+        topContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        topContainerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        topContainerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        topContainerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -(height-width)).isActive = true
+    }
+    
+    func setBackButtonConstraints(topView: UIView, width: CGFloat) {
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.topAnchor.constraint(equalTo: topView.topAnchor, constant: 20).isActive = true
+        backButton.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 20).isActive = true
+        backButton.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -(width-60)).isActive = true
+    }
+    
+    func setShareButtonConstraints(topView: UIView, width: CGFloat) {
+        shareButton.translatesAutoresizingMaskIntoConstraints = false
+        shareButton.topAnchor.constraint(equalTo: topView.topAnchor, constant: 20).isActive = true
+        shareButton.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: width-60).isActive = true
+        shareButton.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -20).isActive = true
+    }
+    
+    func setCardDetailViewConstraints(topView: UIView) {
+        cardDetailView.translatesAutoresizingMaskIntoConstraints = false
+        cardDetailView.topAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
+        cardDetailView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        cardDetailView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        cardDetailView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    func setPageControlConstraints(topView: UIView) {
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.topAnchor.constraint(equalTo: topView.bottomAnchor, constant: -50).isActive = true
+        pageControl.centerXAnchor.constraint(equalTo: topView.centerXAnchor).isActive = true
+    }
+    
+    @objc func moveToHomeView() {
+//        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func popoverModal() {
+//        var vc = UIViewController()
+//        self.present(vc, animated: true)
     }
 }
 
 extension CardDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    enum Section: Int {
+        case site
+        case title
+        case keywordsCell
+        case story
+        case map
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let site: Int = 0
-        let title: Int = 1
-        let keywordsCell: Int = 2
-        let description: Int = 3
-        let map: Int = 4
-        
         let width = collectionView.frame.width - 10
         let keywordWidth = collectionView.frame.width / 4 - 10
         
@@ -175,15 +219,15 @@ extension CardDetailViewController: UICollectionViewDelegate, UICollectionViewDa
         )
         
         switch (indexPath.section) {
-        case site:
+        case Section.site.rawValue:
             return CGSize(width: width, height: 50)
-        case title:
+        case Section.title.rawValue:
             return CGSize(width: width, height: 50)
-        case keywordsCell:
+        case Section.keywordsCell.rawValue:
             return CGSize(width: keywordWidth, height: keywordWidth)
-        case description:
+        case Section.story.rawValue:
             return CGSize(width: width, height: cellSize.height + 20)
-        case map:
+        case Section.map.rawValue:
             return CGSize(width: width, height: 100)
         default:
             return CGSize(width: 0, height: 0)
@@ -191,22 +235,16 @@ extension CardDetailViewController: UICollectionViewDelegate, UICollectionViewDa
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let site: Int = 0
-        let title: Int = 1
-        let keywordsCell: Int = 2
-        let description: Int = 3
-        let map: Int = 4
-        
         switch (section) {
-        case site:
+        case Section.site.rawValue:
             return 1
-        case title:
+        case Section.title.rawValue:
             return 1
-        case keywordsCell:
+        case Section.keywordsCell.rawValue:
             return keywords.keywords.count
-        case description:
+        case Section.story.rawValue:
             return 1
-        case map:
+        case Section.map.rawValue:
             return 1
         default:
             return 0
@@ -223,27 +261,21 @@ extension CardDetailViewController: UICollectionViewDelegate, UICollectionViewDa
         let contentsCell = cardDetailView.dequeueReusableCell(withReuseIdentifier: "contentsCell", for: indexPath) as? ContentsCell
         let mapCell = cardDetailView.dequeueReusableCell(withReuseIdentifier: "mapCell", for: indexPath) as? MapCell
         
-        let site: Int = 0
-        let title: Int = 1
-        let keywordsCell: Int = 2
-        let description: Int = 3
-        let map: Int = 4
-        
         switch (indexPath.section) {
-        case site:
+        case Section.site.rawValue:
             contentsCell?.contentsLabel.text = "어딘가의 장소"
             return contentsCell ?? UICollectionViewCell()
-        case title:
+        case Section.title.rawValue:
             contentsCell?.contentsLabel.text = "대충 제목"
             return contentsCell ?? UICollectionViewCell()
-        case keywordsCell:
+        case Section.keywordsCell.rawValue:
             keywordCell?.keywordLabel.text = keywords.keywords[indexPath.row]
             keywordCell?.backgroundColor = .cyan
             return keywordCell ?? UICollectionViewCell()
-        case description:
+        case Section.story.rawValue:
             contentsCell?.contentsLabel.text = contents
             return contentsCell ?? UICollectionViewCell()
-        case map:
+        case Section.map.rawValue:
             mapCell?.moveLocation(latitudeValue: 37.334754657382234, longitudeValue: -122.00898272593827, delta: 0.01)
             mapCell?.setAnnotation(latitudeValue: 37.334754657382234, longitudeValue: -122.00898272593827, delta: 0.01, title: "Apple Park", subtitle: "aa")
             return mapCell ?? UICollectionViewCell()
@@ -258,10 +290,9 @@ extension CardDetailViewController: UICollectionViewDelegate, UICollectionViewDa
         guard kind == UICollectionView.elementKindSectionHeader,
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header", for: indexPath) as? HeaderView
         else { return UICollectionReusableView() }
-        let description: Int = 3
         
         switch (indexPath.section) {
-        case description:
+        case Section.story.rawValue:
             header.backgroundColor = .red
             header.prepare(text: "함께 전하는 이야기")
             return header
