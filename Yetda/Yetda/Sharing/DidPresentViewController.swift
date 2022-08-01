d0d0ba3dc13b06dab1d7e020e5c5508c659ed695
rbwo552 <rbwo552@naver.com>
2022-08-01 17:02:55 +0900

    Feat : 선물 완료 뷰에서 홈뷰로 가는 기능 추가

--- Yetda/Yetda/Sharing/DidPresentViewController.swift
+++ Yetda/Yetda/Sharing/DidPresentViewController.swift
@@ -14,24 +14,18 @@ class DidPresentViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         backgroundImage.image = UIImage(named: "background")
     }
     
     @IBAction func clickedBackButton(_ sender: Any) {
-        // TODO: 요기 밑에 선물 했다는 Flag를 다시 바꿔주면 될 듯 -
-        // dismiss 클로저 ?
-        // ViewWillDisAppear ?
-        // viewDidDisappear ?
         dismiss(animated: true)
     }
     
     @IBAction func clickedNextButton(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "DidCardShare", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "DidCardShareViewController") as! DidCardShareViewController
-        
-        /// 뒤에 뷰 계층이 없이 새로 아래 뷰 부터 계층이 시작됨
-        vc.modalPresentationStyle = .fullScreen
-        self.present(vc, animated: true)
+        // MARK: HomeView로 가는 버튼 구현
+        guard let pvc = self.presentingViewController as? UINavigationController else { return }
+        self.dismiss(animated: false) {
+            pvc.popViewController(animated: false)
+        }
     }
 }
