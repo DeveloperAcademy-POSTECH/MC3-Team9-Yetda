da5a2becd4b9a043b8e799259c89bc7cb93bb3cc
Valentino1994 <demua1994@gmail.com>
2022-08-01 15:49:19 +0900

    Feat[#78] : 회원탈퇴 기능 구현하였습니다.

--- Yetda/Yetda/MyPage/MyPageViewController.swift
+++ Yetda/Yetda/MyPage/MyPageViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SafariServices
+import FirebaseAuth
 
 class MyPageViewController: UIViewController {
     
@@ -29,6 +30,14 @@ class MyPageViewController: UIViewController {
         let logoutalert = UIAlertController(title: "로그아웃 확인", message: "로그아웃 하시겠어요?", preferredStyle: .alert)
         let cancle = UIAlertAction(title: "취소", style: .default, handler: nil)
         let logout = UIAlertAction(title: "로그아웃", style: .destructive) { UIAlertAction in
+            let firebaseAuth = Auth.auth()
+            
+            do {
+                try firebaseAuth.signOut()
+            } catch let signOutError as NSError {
+                print("ERROR: singout \(signOutError.localizedDescription)")
+            }
+            
             print("로그아웃 로직 짜야함")
         }
         logoutalert.addAction(cancle)
@@ -39,7 +48,7 @@ class MyPageViewController: UIViewController {
     @IBAction func forResginButton(_ sender: UIButton) {
         let resignAlert = UIAlertController(title: "회원탈퇴 확인", message: "회원탈퇴시 지금까지 기록된 데이터가 사라집니다", preferredStyle: .alert)
         let cancle = UIAlertAction(title: "취소", style: .default, handler: nil)
-        let resign = UIAlertAction(title: "로그아웃", style: .destructive) { UIAlertAction in
+        let resign = UIAlertAction(title: "", style: .destructive) { UIAlertAction in
             print("회원탈퇴 로직 짜야함")
         }
         resignAlert.addAction(cancle)
